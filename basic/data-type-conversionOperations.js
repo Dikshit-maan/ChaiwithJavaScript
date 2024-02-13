79049f1be250c6c36bfa32fa50c24b00e06e6a8b
Dikshit-maan <149257007+Dikshit-maan@users.noreply.github.com>
2024-02-13 06:55:29 +0000

    data-type conversions Operations

---
+++ basic/data-type-conversionOperations.js
@@ -0,0 +1,34 @@
+let score = "23abc"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let myScore = Number(score);
+// console.log(typeof myScore);
+// console.log(myScore);
+
+//"33" = 33
+// "33abc" = NaN
+//true = 1
+//false = 0
+
+let fullName = 1
+
+let changeInBool = Boolean(fullName)
+// console.log(typeof changeInBool);
+// console.log(changeInBool);
+
+// 1 = true;
+// 0 = false;
+// "dikshit" = true
+// "" = false
+
+
+let sum = .009
+
+let totalSum = String(sum)
+console.log(typeof totalSum);
+console.log(totalSum);
+
+// true = true;(string)
+// false = false;(string)
+// 33 = 33(string)
